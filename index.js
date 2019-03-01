d3a652f676cd57461f61d12f9f4fe902de9ebbed
Ken <34059898+thehydrogen@users.noreply.github.com>
2019-03-01 17:35:31 +0100

    📝 Main file developments

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = async (client, evalString, clientName) => {
-    // Under Construction
+module.exports = async (client, evalString, evalResult, clientName) => {
+    if (evalString.includes(`${clientName}.token`)) {
+        return 1;
+    }
+    if (evalString.includes('token')) {
+        return 1;
+    }
+    if (evalResult.includes(client.token)) {
+        return 1;
+    }
+    if (evalString.includes('client[') && evalString.includes('t')) {
+        return 1;
+    }
+    
+    return 0;
 };
